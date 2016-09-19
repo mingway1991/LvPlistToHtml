9f0535bcf848e324e812fdc4bdab4ebec346427a
石茗伟 <shimingwei@shimingweideiMac.local>
2016-09-19 15:04:59 +0800

    first commit

---
+++ LVPlistToHtml/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LVPlistToHtml
+//
+//  Created by 石茗伟 on 16/9/19.
+//  Copyright © 2016年 驴妈妈. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
